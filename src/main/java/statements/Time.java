9e8499596f960706c458fcc5dedf534d32b93910
Bern00 <ecsbern@gmail.com>
2020-11-12 14:25:04 +0100

    Csomagok

---
+++ src/main/java/statements/Time.java
@@ -0,0 +1,36 @@
+package statements;
+
+public class Time {
+    private int hours;
+    private int minutes;
+    private int seconds;
+
+    public Time(int hours, int minutes, int seconds) {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
+
+    public int getInMinutes() {
+        return minutes + hours * 60;
+    }
+
+    public int getInSeconds() {
+        return hours * 3600 + minutes * 60 + seconds;
+    }
+
+    public boolean earlierThan(Time other) {
+        int thisTimeInSeconds = this.getInSeconds();
+        int otherTimeInSeconds = other.getInSeconds();
+        return thisTimeInSeconds < otherTimeInSeconds;
+    }
+
+    @Override
+    public String toString() {
+        return "Time{" +
+                "hours=" + hours +
+                ", minutes=" + minutes +
+                ", seconds=" + seconds +
+                '}';
+    }
+}
---
+++ src/main/java/statements/TimeMain.java
@@ -0,0 +1,12 @@
+package statements;
+
+import java.util.Scanner;
+
+public class TimeMain {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextInt();
+
+    }
+}
