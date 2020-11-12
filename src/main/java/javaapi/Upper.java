f2a8e53fcdf84aad4fd63e494b997e03ad4bd0c4
Bern00 <ecsbern@gmail.com>
2020-11-12 14:53:04 +0100

    JAVA api

--- src/main/java/javaapi/Upper.java
+++ src/main/java/javaapi/Upper.java
@@ -3,7 +3,7 @@ package javaapi;
 public class Upper {
     public static void main(String[] args) {
 
-        System.out.println("Hello World!".toUpperCase());
+        System.out.println("Hello World!!".toUpperCase());
 
     }
 
