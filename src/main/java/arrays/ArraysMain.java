928d2b5779cc59eb335b57aaa5f618712e0a85e7
Bern00 <ecsbern@gmail.com>
2020-11-19 10:54:07 +0100

    tombok kezelese

---
+++ src/main/java/arrays/ArraysMain.java
@@ -0,0 +1,23 @@
+package arrays;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArraysMain {
+
+    public String numberOfDaysAsString() {
+        int numberOfDays[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        return Arrays.toString(numberOfDays);
+    }
+
+    public List<String> daysOfWeek() {
+        String names[] = {"Hétfő", "Kedd", "Szerda", "Csütörtök", "Péntek", "Szombat", "Vasárnap"};
+        return Arrays.asList(names);
+    }
+
+    public static void main(String[] args) {
+        ArraysMain arraysMain = new ArraysMain();
+        System.out.println(arraysMain.numberOfDaysAsString());
+        System.out.println(arraysMain.daysOfWeek());
+    }
+}
