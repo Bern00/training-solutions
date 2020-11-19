55994c8e2691b85406987deccaf9092a8a46af8d
Bern00 <ecsbern@gmail.com>
2020-11-19 19:43:41 +0100

    list

---
+++ src/main/java/arraylist/Capsules.java
@@ -0,0 +1,41 @@
+package arraylist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Capsules {
+
+    List<String> colors = new ArrayList<>();
+
+    public void addLast(String color) {
+        colors.add(color);
+    }
+
+    public void addFirst(String color) {
+        colors.add(0, color);
+    }
+
+    public void removeFirst() {
+        colors.remove(0);
+    }
+
+    public void removeLast() {
+        colors.remove(colors.size() - 1);
+    }
+
+    public List<String> getColors() {
+        return colors;
+    }
+
+    public static void main(String[] args) {
+        Capsules capsules = new Capsules();
+        capsules.addLast("Green");
+        capsules.addLast("Yellow");
+        capsules.addLast("White");
+        capsules.addFirst("Blue");
+        capsules.removeFirst();
+        capsules.removeLast();
+
+        System.out.println(capsules.getColors());
+    }
+}
