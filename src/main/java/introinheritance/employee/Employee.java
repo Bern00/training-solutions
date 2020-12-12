e2eda63a1ac1add44814d996ab4762414d44399d
Bern00 <ecsbern@gmail.com>
2020-12-12 21:41:28 +0100

    employee

---
+++ src/main/java/introinheritance/employee/Employee.java
@@ -0,0 +1,29 @@
+package introinheritance.employee;
+
+public class Employee {
+
+    private String name;
+    private String occupation;
+    private int payment;
+
+    public Employee(String name, String occupation, int payment) {
+
+        if(name.equals("")
+                || name.equals(null)
+                || occupation.equals("")
+                || occupation.equals(null)
+                || payment%1000 != 0) {
+            throw new IllegalArgumentException("Not valid information");
+        }
+
+        this.name = name;
+        this.occupation = occupation;
+        this.payment = payment;
+    }
+
+    public String toString() {
+        return name + " - " + occupation + " - " + payment +" Ft";
+    }
+
+    
+}
---
+++ src/test/java/introinheritance/employee/EmployeeTest.java
@@ -0,0 +1,23 @@
+package introinheritance.employee;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class EmployeeTest {
+
+    @Test
+    public void testTestToString() {
+        //Given
+        Employee employee = new Employee("Kis Géza", "földműves", 872000);
+
+        //When
+        String kontatenal = employee.toString();
+
+        //Then
+        assertThat(kontatenal, equalTo("Kis Géza - földműves - 872000 Ft"));
+    }
+
+}
