23f46ab3b8b02a45348e028c14c86043187ac04b
Bern00 <ecsbern@gmail.com>
2021-01-26 18:13:39 +0100

    default interfaces

---
+++ src/test/java/interfacemethods/PrintableTest.java
@@ -0,0 +1,15 @@
+package interfacemethods;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PrintableTest {
+
+    @Test
+    public void testGetColorWithDefaultImplementation() {
+        Printable printable = new TestPrintable();
+        assertEquals(Printable.BLACK, printable.getColor(0));
+    }
+
+}
