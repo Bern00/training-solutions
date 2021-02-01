8e4b21a57509044fa31237bd722bdd397fb05dca
Bern00 <ecsbern@gmail.com>
2021-02-01 09:34:56 +0100

    sajat kivetel

---
+++ src/main/java/exceptionclass/bankotherexceptions/LowBalanceBankOperationException.java
@@ -0,0 +1,10 @@
+package exceptionclass.bankotherexceptions;
+
+public class LowBalanceBankOperationException extends InvalidBankOperationException{
+    public LowBalanceBankOperationException() {
+    }
+
+    public LowBalanceBankOperationException(String message) {
+        super(message);
+    }
+}
